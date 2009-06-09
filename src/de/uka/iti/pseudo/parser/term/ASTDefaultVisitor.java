fe3cd7fb1e5108af8766a3da928b0adb17471c18
Mattias Ulbrich <mulbrich@ira.uka.de>
2009-06-09 19:13:20 +0200

    SVN commit 337

---
+++ src/de/uka/iti/pseudo/parser/term/ASTDefaultVisitor.java
@@ -0,0 +1,73 @@
+package de.uka.iti.pseudo.parser.term;
+
+import de.uka.iti.pseudo.parser.ASTVisitException;
+
+public abstract class ASTDefaultVisitor implements ASTVisitor {
+
+    protected abstract void defaultVisit(ASTElement term) throws ASTVisitException;
+
+    public void visit(ASTApplicationTerm applicationTerm)
+            throws ASTVisitException {
+        defaultVisit(applicationTerm);
+    }
+
+    public void visit(ASTBinderTerm binderTerm) throws ASTVisitException {
+        defaultVisit(binderTerm);
+    }
+
+    public void visit(ASTIdentifierTerm identifierTerm)
+            throws ASTVisitException {
+        defaultVisit(identifierTerm);
+    }
+
+    public void visit(ASTListTerm listTerm) throws ASTVisitException {
+        defaultVisit(listTerm);
+    }
+
+    public void visit(ASTModalityTerm modalityTerm) throws ASTVisitException {
+        defaultVisit(modalityTerm);
+    }
+
+    public void visit(ASTModAssignment modAssignment) throws ASTVisitException {
+        defaultVisit(modAssignment);
+    }
+
+    public void visit(ASTModCompound modCompound) throws ASTVisitException {
+        defaultVisit(modCompound);
+    }
+
+    public void visit(ASTModIf modIf) throws ASTVisitException {
+        defaultVisit(modIf);
+    }
+
+    public void visit(ASTModSkip modSkip) throws ASTVisitException {
+        defaultVisit(modSkip);
+    }
+
+    public void visit(ASTModWhile modWhile) throws ASTVisitException {
+        defaultVisit(modWhile);
+    }
+
+    public void visit(ASTNumberLiteralTerm numberLiteralTerm)
+            throws ASTVisitException {
+        defaultVisit(numberLiteralTerm);
+    }
+
+    public void visit(ASTTypeRef typeRef) throws ASTVisitException {
+        defaultVisit(typeRef);
+    }
+
+    public void visit(ASTOperatorIdentifierTerm operatorIdentifierTerm)
+            throws ASTVisitException {
+        defaultVisit(operatorIdentifierTerm);
+    }
+
+    public void visit(ASTFixTerm infixTerm) throws ASTVisitException {
+        defaultVisit(infixTerm);
+    }
+
+    public void visit(ASTAsType asType) throws ASTVisitException {
+        defaultVisit(asType);
+    }
+
+}
