7db1b7bc3055a4abf4e5803b49320ccbb6488960
Mattias Ulbrich <mulbrich@ira.uka.de>
2009-10-21 16:20:18 +0200

    pretty print field names @h is now supressed

--- core/test/HeapPrettyPrinter.java
+++ core/test/HeapPrettyPrinter.java
@@ -1,17 +1,21 @@
 package test;
 
+import de.uka.iti.pseudo.environment.Function;
 import de.uka.iti.pseudo.prettyprint.PrettyPrintPlugin;
 import de.uka.iti.pseudo.term.Application;
 import de.uka.iti.pseudo.term.Binding;
-import de.uka.iti.pseudo.term.Term;
 import de.uka.iti.pseudo.term.TermException;
-import de.uka.iti.pseudo.term.statement.AssignmentStatement;
+import de.uka.iti.pseudo.term.Type;
+import de.uka.iti.pseudo.term.TypeApplication;
 
 public class HeapPrettyPrinter extends PrettyPrintPlugin {
 
     @Override public void prettyPrintTerm(Application application) throws TermException {
         
-        if ("sel".equals(application.getFunction().getName())) {
+        Function function = application.getFunction();
+        String name = function.getName();
+        
+        if ("sel".equals(name)) {
 
             // obj
             printSubterm(application, 1);
@@ -19,14 +23,14 @@ public class HeapPrettyPrinter extends PrettyPrintPlugin {
             // field
             printSubterm(application, 2);
 
-            if(!"h".equals(application.toString(false))) {
+            if(!"h".equals(application.getSubterm(0).toString(false))) {
                 append("@");
                 // heap
                 printSubterm(application, 0);
             }
         } else
 
-        if ("stor".equals(application.getFunction().getName())) {
+        if ("stor".equals(name)) {
             append("{");
             // obj
             printSubterm(application, 1);
@@ -39,8 +43,27 @@ public class HeapPrettyPrinter extends PrettyPrintPlugin {
             append("}@");
             // heap
             printSubterm(application, 0);
-        }
+        } else
         
+        if(isFieldType(function.getResultType()) && function.getArity() == 0 &&
+                name.startsWith("field_")) {
+            int last_ = name.lastIndexOf('_');
+            append(name.substring(last_+1));
+        }
+                
+        
+    }
+
+    /*
+     * can this type be unified with "field('a)"?
+     */
+    private boolean isFieldType(Type type) {
+        if (type instanceof TypeApplication) {
+            TypeApplication tyApp = (TypeApplication) type;
+            if("field".equals(tyApp.getSort().getName()))
+                return true;
+        }
+        return false;
     }
 
     @Override public void prettyPrintTerm(Binding term) {
