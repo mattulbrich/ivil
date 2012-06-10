c701921b607c7527364603430142eade7cbf4b84
Mattias Ulbrich <ulbrich@kit.edu>
2012-06-10 17:54:28 +0200

    introduction of the new nonnull-system

--- modules/core/src/nonnull/NonNull.java
+++ modules/core/src/nonnull/NonNull.java
@@ -1,11 +1,9 @@
 /*
- * This file is part of
- *    ivil - Interactive Verification on Intermediate Language
+ * NonNull Runtime Checking for Methods
  *
- * Copyright (C) 2009-2012 Karlsruhe Institute of Technology
+ * 2009 by Mattias Ulbrich
  *
- * The system is protected by the GNU General Public License.
- * See LICENSE.TXT (distributed with this file) for details.
+ * published under GPL.
  */
 
 package nonnull;
@@ -13,24 +11,27 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-
 /**
  * The annotation <code>NonNull</code> can be used to annotate methods or
- * their parameters (of reference type) to denote that the may not be null.
- * 
+ * their parameters (of reference type) to denote that the must not be null.
+ *
+ * If a class is annotated NonNull, all parameters and returns to all methods
+ * (and constructors!) are considered annotated NonNull. You can declare
+ * exceptions to this default using the {@link Nullable} annotation.
+ *
  * The annotations are documented in JavaDoc.
- * 
+ *
  * You can patch class files containing NonNull annotations with runtime checks
  * using the class {@link NonNullPatch}
- * 
+ *
  * You can use the classloader {@link NonNullClassLoader} to patch class files
  * dynamically at run time.
  */
 
 public
-  @Documented 
-  @Retention(RetentionPolicy.CLASS) 
-  
+  @Documented
+  @Retention(RetentionPolicy.RUNTIME)
+
 @interface NonNull {
 
 }
--- modules/core/src/nonnull/Nullable.java
+++ modules/core/src/nonnull/Nullable.java
@@ -1,11 +1,9 @@
 /*
- * This file is part of
- *    ivil - Interactive Verification on Intermediate Language
+ * NonNull Runtime Checking for Methods
  *
- * Copyright (C) 2009-2012 Karlsruhe Institute of Technology
+ * 2009 by Mattias Ulbrich
  *
- * The system is protected by the GNU General Public License.
- * See LICENSE.TXT (distributed with this file) for details.
+ * published under GPL.
  */
 
 package nonnull;
@@ -19,20 +17,20 @@ import java.lang.annotation.RetentionPolicy;
  * their parameters (of reference type) to denote that the *may* be null.
  * If a class is annotated NonNull, this annotation can be used to declare
  * exception of this default behaviour.
- * 
+ *
  * The annotations are documented in JavaDoc.
- * 
+ *
  * You can patch class files containing NonNull annotations with runtime checks
  * using the class {@link NonNullPatch}
- * 
+ *
  * You can use the classloader {@link NonNullClassLoader} to patch class files
  * dynamically at run time.
  */
 
 public
-  @Documented 
-  @Retention(RetentionPolicy.CLASS) 
-  
+  @Documented
+  @Retention(RetentionPolicy.RUNTIME)
+
 @interface Nullable {
 
 }
