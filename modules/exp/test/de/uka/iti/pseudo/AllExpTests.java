552dc8bd993e62ad1d9b7d070a66a6b3bae1ff5d
Mattias Ulbrich <ulbrich@kit.edu>
2013-09-19 12:19:37 +0200

    moving algo packages to own module

--- modules/exp/test/de/uka/iti/pseudo/AllExpTests.java
+++ modules/exp/test/de/uka/iti/pseudo/AllExpTests.java
@@ -2,10 +2,6 @@ package de.uka.iti.pseudo;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import de.uka.iti.pseudo.algo.TestAlgoVisitor;
-import de.uka.iti.pseudo.algo.TestChainedRelationVisitor;
-import de.uka.iti.pseudo.algo.TestParser;
-import de.uka.iti.pseudo.algo.TestTermVisitor;
 import de.uka.iti.pseudo.auto.strategy.TestInstantiationStrategy;
 import de.uka.iti.pseudo.rule.meta.TestRefinementModifier;
 
@@ -14,10 +10,6 @@ public class AllExpTests {
     public static Test suite() {
         TestSuite suite = new TestSuite(AllExpTests.class.getName());
         //$JUnit-BEGIN$
-        suite.addTestSuite(TestAlgoVisitor.class);
-        suite.addTestSuite(TestChainedRelationVisitor.class);
-        suite.addTestSuite(TestParser.class);
-        suite.addTestSuite(TestTermVisitor.class);
         suite.addTestSuite(TestRefinementModifier.class);
         suite.addTestSuite(TestInstantiationStrategy.class);
         //$JUnit-END$
